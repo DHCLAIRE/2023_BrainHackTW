f8d09e39f7555acd407eaedb6b7e56b888b0ca06
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-09 20:34:51 +0800

    Create Module6_useful_functions.py

---
+++ Module6_useful_functions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+import os
+from gtts import gTTS
+import pandas as pd
+import time
+from pathlib import Path
+import nltk
+import re
+#from nltk import sent_tokenize
+#from nltk import tokenize
+
+if __name__ == "__main__":
