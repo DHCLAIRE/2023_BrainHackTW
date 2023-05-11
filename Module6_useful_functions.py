9c817c6d3697d79eee654c55900d8b1f419f8725
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-11 16:54:00 +0800

    Add required functions

--- Module6_useful_functions.py
+++ Module6_useful_functions.py
@@ -17,5 +17,58 @@ import argparse
 #from nltk import sent_tokenize
 #from nltk import tokenize
 
-if __name__ == "__main__":
+def encrypt_messege(letterSTR, keySTR):
+    '''
+    To encryt the messege by using the key (in unicode form)
+    , hence return a new representation as the result. 
+    e.g. messege = "l"; key = "h"; return = "Ô"
+    '''
+    letterSTR_indexINT = ord(letterSTR)
+    keySTR_indexINT = ord(keySTR)
     
+    # get the encrypted (result) index by get the residue of (letterSTR_indexINT + keySTR_indexINT) / 1114112
+    encrypted_indexINT = (letterSTR_indexINT + keySTR_indexINT) % 1114112  
+    
+    return chr(encrypted_indexINT) # characterize the result from its unicode sequence
+
+def decrypt_messege(letterSTR, keySTR):
+    '''
+    To decryt the messege by using the key (in unicode form)
+    , hence return a new representation as the result. 
+    e.g. key = "h"; messege = "l"; return = "Ô"
+    '''
+    letterSTR_indexINT = ord(letterSTR)
+    keySTR_indexINT = ord(keySTR)
+    
+    # get the encrypted (result) index by get the residue of (letterSTR_indexINT - keySTR_indexINT) / 1114112  # Why minus??
+    decrypted_indexINT = (letterSTR_indexINT - keySTR_indexINT) % 1114112
+    
+    return chr(decrypted_indexINT) # characterize the result from its unicode sequence
+
+def process_messege(letterSTR, keySTR, encryptionBOOL):
+    '''
+    To process the target messege and verify the encryption correctness
+    , hence return a conformation strings to answer the test acomplishment.
+    e.g. messege = "l"; key = "h"; return = "Ô", encryptionBOOL = True   >>??
+    '''
+    target_message = ""
+    if encryptionBOOL == True:
+        target_message = encrypt_messege(letterSTR, keySTR)
+    else:
+        target_message = decrypt_messege(letterSTR, keySTR)  # correct??
+    
+    
+    
+    
+    letterSTR_indexINT = ord(letterSTR)
+    keySTR_indexINT = ord(keySTR)
+    
+    # get the encrypted (result) index by get the residue of letterSTR_indexINT/keySTR_indexINT
+    encrypted_indexINT = (letterSTR_indexINT + keySTR_indexINT) % 1114112  
+    
+    return chr(encrypted_indexINT) # characterize the result from its unicode sequence
+
+
+if __name__ == "__main__":
+    testSTR = encrypt_messege("k", "o")
+    print(testSTR)  # Is it like to use whatever key to produce/protext the message?
