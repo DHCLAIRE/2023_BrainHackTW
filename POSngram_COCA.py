0a27c3f22142ea51396f58676072dec5a7ef8115
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-24 09:23:11 +0800

    Add glob() to search all desired files

--- POSngram_COCA.py
+++ POSngram_COCA.py
@@ -7,7 +7,7 @@ import json
 import random
 from random import sample
 import os
-from gtts import gTTS
+#from gtts import gTTS
 import pandas as pd
 import time
 from pathlib import Path
@@ -15,20 +15,46 @@ import nltk
 import re
 #from nltk import sent_tokenize
 #from nltk import tokenize
+import glob
 
 
-def LISTblankEraser(rawLIST):
-    '''
-    Remove the blank that inside the list
-    '''
-    newrawLIST = []
-    for row in rawLIST:
-        if len(row) == 0:
-            rawLIST.pop(rawLIST.index(row))
-        else:
-            pass
-        newrawLIST = rawLIST
-        #print(len(newrawLIST))
-        return newrawLIST
+# Command examples from https://kristopherkyle.github.io/corpus-analysis-python/Python_Tutorial_4.html
+def corpus_freq(dir_name,lemma_d):
+    freq = {} #create an empty dictionary to store the word : frequency pairs
+
+    #create a list that includes all files in the dir_name folder that end in ".txt"
+    filenames = glob.glob(dir_name + "/*.txt")
+
+    #iterate through each file:
+    for filename in filenames:
+        #open the file as a string
+        text = open(filename, errors = "ignore").read()
+        #tokenize text using our tokenize() function
+        tokenized = tokenize(text)
+        #lemmatize text using the lemmatize() function
+        lemmatized = lemmatize(tokenized,lemma_d)
+
+        #iterate through the lemmatized text and add words to the frequency dictionary
+        for x in lemmatized:
+            #the first time we see a particular word we create a key:value pair
+            if x not in freq:
+                freq[x] = 1
+            #when we see a word subsequent times, we add (+=) one to the frequency count
+            else:
+                freq[x] += 1
+
+    return(freq)
 
 if __name__ == "__main__":
+    # Go through all the files
+    txt_DIR = "/Users/neuroling/Documents/coca/coca-wlp/wlp_acad_vuw"
+    filenamesLIST = glob.glob(txt_DIR +"/*.txt")
+    print(type(filenamesLIST))
+    print(len(filenamesLIST))
+    
+    for fileN_STR in filenamesLIST:
+        with open (fileNamesSTR, errors="ignore", encoding="utf-8") as fileTXT:
+            fileTXT.read()
+            
+        
+    
