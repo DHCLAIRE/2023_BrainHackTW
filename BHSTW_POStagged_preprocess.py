c969594fd2f769fd07f4befb6d3025c4149f94bf
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-29 22:08:13 +0800

    Create BHSTW_POStagged_preprocess.py

---
+++ BHSTW_POStagged_preprocess.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+import os
+#from gtts import gTTS
+#import pandas as pd
+#import time
+
+from pathlib import Path
+import nltk
+import re
+from collections import Counter
+from nltk import ngrams
+#from nltk import sent_tokenize
+from nltk import tokenize
+import glob
+
+if __name__ == "__main__":
+    
+    
