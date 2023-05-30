1effaa82f104cd929d5a375a1c252d03b5c1ec6b
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-30 09:13:59 +0800

    Add checking the txt content commands

--- BHSTW_POStagged_preprocess.py
+++ BHSTW_POStagged_preprocess.py
@@ -22,4 +22,20 @@ import glob
 
 if __name__ == "__main__":
     
+    txtRoot_DIR_STR = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/"
+    POStaggedFolder = txtRoot_DIR_STR + "LTTC_Stim_POStagged"
+    #print(POStaggedFolder)
+    filenamesLIST = glob.glob(POStaggedFolder + "/*.txt")
+    #print(filenamesLIST)
     
+    # Open evey tagged files
+    for fileN_STR in filenamesLIST[:3]:
+        with open (fileN_STR, errors="ignore", encoding="utf-8") as fileTXT:  # Use all "wlp-" tagged txt files, it contains POS taggings
+            rawLIST = fileTXT.read().split("\n")  #.replace("\t", " ")
+            pprint(rawLIST[:10])
+            print(type(rawLIST))
+            print(len(rawLIST))
+            for row in rawLIST:
+                print(row)
+                print(type(row))
+                print(len(row))
