1bc8337d00dfb75c0fcbb4094d94de21d75b2094
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-31 12:33:39 +0800

    Use re to exclude the space in the text

--- BHSTW_POStagged_preprocess.py
+++ BHSTW_POStagged_preprocess.py
@@ -20,6 +20,17 @@ from nltk import ngrams
 from nltk import tokenize
 import glob
 
+def space2comma(strLIST):
+    '''
+    Turn the space within the string into comma
+    '''
+    turedLIST = []
+    for elementSTR in strLIST:
+        turedLIST.extend(elementSTR.split(" "))
+        #print(turedLIST)
+    return turedLIST
+
+
 if __name__ == "__main__":
     
     txtRoot_DIR_STR = "/Volumes/Neurolang_1/Project_Assistant/2021_Ongoing/2020_LTTC/Experiment_materials/LTTC_MEG/"
@@ -33,9 +44,20 @@ if __name__ == "__main__":
         with open (fileN_STR, errors="ignore", encoding="utf-8") as fileTXT:  # Use all "wlp-" tagged txt files, it contains POS taggings
             rawLIST = fileTXT.read().split("\n")  #.replace("\t", " ")
             pprint(rawLIST[:10])
-            print(type(rawLIST))
-            print(len(rawLIST))
-            for row in rawLIST:
-                print(row)
-                print(type(row))
-                print(len(row))
+            #print(type(rawLIST))
+            #print(len(rawLIST))
+            cleanedLIST = []
+        #print("ORI_", len(rawLIST))
+        for rowSTR in rawLIST:
+            if "---" in rowSTR:
+                #print(rowSTR)
+                rawLIST.remove(rowSTR)
+            else:
+                pass
+        #print("NEW_", len(rawLIST)
+            de_rowLIST = re.findall(r'\S+', row)  # [\s] = find space  ; \d+\s\d+ = find all set of (two strings of digits with " one" space in between); de = denoised strings
+            print("de_rowLIST", de_rowLIST)
+            cleanedLIST.append(de_rowLIST)
+
+        print(len(cleanedLIST))
+                
