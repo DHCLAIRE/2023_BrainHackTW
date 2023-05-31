f74e0e5517283510b4c8959d23da66d1ff050168
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-05-31 12:41:39 +0800

    Add remove first & second line in the txt file

--- BHSTW_POStagged_preprocess.py
+++ BHSTW_POStagged_preprocess.py
@@ -43,21 +43,27 @@ if __name__ == "__main__":
     for fileN_STR in filenamesLIST[:3]:
         with open (fileN_STR, errors="ignore", encoding="utf-8") as fileTXT:  # Use all "wlp-" tagged txt files, it contains POS taggings
             rawLIST = fileTXT.read().split("\n")  #.replace("\t", " ")
+            rawLIST.pop(0) # remove the 1st and the 2nd elements in the LIST
+            rawLIST.pop(0) # and the 2nd elements in the LIST
             pprint(rawLIST[:10])
             #print(type(rawLIST))
             #print(len(rawLIST))
-            cleanedLIST = []
-        #print("ORI_", len(rawLIST))
+        print("ORI_", len(rawLIST))
+        
+        # Remove the segment line in the raw txt file
+        cleanedLIST = []
         for rowSTR in rawLIST:
             if "---" in rowSTR:
                 #print(rowSTR)
                 rawLIST.remove(rowSTR)
             else:
                 pass
-        #print("NEW_", len(rawLIST)
-            de_rowLIST = re.findall(r'\S+', row)  # [\s] = find space  ; \d+\s\d+ = find all set of (two strings of digits with " one" space in between); de = denoised strings
-            print("de_rowLIST", de_rowLIST)
-            cleanedLIST.append(de_rowLIST)
+        print("NEW_", len(rawLIST))
 
+        # Exclude the space in the string, and split them into a collected LIST
+        for n_rowSTR in rawLIST:
+            de_rowLIST = re.findall(r'\S+', n_rowSTR)  # [\s] = find space  ; \d+\s\d+ = find all set of (two strings of digits with " one" space in between); de = denoised strings
+            #print("de_rowLIST", de_rowLIST)
+            cleanedLIST.append(de_rowLIST)
         print(len(cleanedLIST))
                 
